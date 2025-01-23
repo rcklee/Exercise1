c068936d8c8b5cced34e97fcc356852683ee33b2
Viritkarvan V Chum 12th FHHS <101680@SWCSD.US>
2025-01-23 14:35:46 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        String City="Columbus";
+
+        int zip = 43228;
+
+        int[] dailyHighs = {32, 25, 27, 40, 45};
+
+        int length = dailyHighs.length;
+
+        int sum = 0;
+
+        for (int i = 0; i < dailyHighs.length; i++) {
+            sum += dailyHighs[i];
+        }
+
+        double average =  sum/ (double) dailyHighs.length;
+//jhgjg
+        System.out.println("City: "+City+" Zip Code: "+ zip+" Average High Temperature: "+ average);
+    }
+}
+
